1ac0d38297daacab95678540054fb4411e5c9c7a
sam <chenxsan@gmail.com>
2017-02-25 10:35:47 +0800

    remove background

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,11 @@
 // generated on 2017-02-24 using generator-chrome-extension 0.6.1
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import del from 'del';
-import runSequence from 'run-sequence';
-import {stream as wiredep} from 'wiredep';
+import gulp from 'gulp'
+import gulpLoadPlugins from 'gulp-load-plugins'
+import del from 'del'
+import runSequence from 'run-sequence'
+import {stream as wiredep} from 'wiredep'
 
-const $ = gulpLoadPlugins();
+const $ = gulpLoadPlugins()
 
 gulp.task('extras', () => {
   return gulp.src([
@@ -13,26 +13,26 @@ gulp.task('extras', () => {
     'app/_locales/**',
     '!app/scripts.babel',
     '!app/*.json',
-    '!app/*.html',
+    '!app/*.html'
   ], {
     base: 'app',
     dot: true
-  }).pipe(gulp.dest('dist'));
-});
+  }).pipe(gulp.dest('dist'))
+})
 
-function lint(files, options) {
+function lint (files, options) {
   return () => {
     return gulp.src(files)
       .pipe($.eslint(options))
-      .pipe($.eslint.format());
-  };
+      .pipe($.eslint.format())
+  }
 }
 
 gulp.task('lint', lint('app/scripts.babel/**/*.js', {
   env: {
     es6: true
   }
-}));
+}))
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
@@ -44,13 +44,13 @@ gulp.task('images', () => {
       svgoPlugins: [{cleanupIDs: false}]
     }))
     .on('error', function (err) {
-      console.log(err);
-      this.end();
+      console.log(err)
+      this.end()
     })))
-    .pipe(gulp.dest('dist/images'));
-});
+    .pipe(gulp.dest('dist/images'))
+})
 
-gulp.task('html',  () => {
+gulp.task('html', () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.sourcemaps.init())
@@ -58,39 +58,33 @@ gulp.task('html',  () => {
     .pipe($.if('*.css', $.cleanCss({compatibility: '*'})))
     .pipe($.sourcemaps.write())
     .pipe($.if('*.html', $.htmlmin({removeComments: true, collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('chromeManifest', () => {
   return gulp.src('app/manifest.json')
     .pipe($.chromeManifest({
-      buildnumber: true,
-      background: {
-        target: 'scripts/background.js',
-        exclude: [
-          'scripts/chromereload.js'
-        ]
-      }
-  }))
+      buildnumber: true
+    }))
   .pipe($.if('*.css', $.cleanCss({compatibility: '*'})))
   .pipe($.if('*.js', $.sourcemaps.init()))
   .pipe($.if('*.js', $.uglify()))
   .pipe($.if('*.js', $.sourcemaps.write('.')))
-  .pipe(gulp.dest('dist'));
-});
+  .pipe(gulp.dest('dist'))
+})
 
 gulp.task('babel', () => {
   return gulp.src('app/scripts.babel/**/*.js')
       .pipe($.babel({
         presets: ['es2015']
       }))
-      .pipe(gulp.dest('app/scripts'));
-});
+      .pipe(gulp.dest('app/scripts'))
+})
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']))
 
 gulp.task('watch', ['lint', 'babel'], () => {
-  $.livereload.listen();
+  $.livereload.listen()
 
   gulp.watch([
     'app/*.html',
@@ -98,38 +92,38 @@ gulp.task('watch', ['lint', 'babel'], () => {
     'app/images/**/*',
     'app/styles/**/*',
     'app/_locales/**/*.json'
-  ]).on('change', $.livereload.reload);
+  ]).on('change', $.livereload.reload)
 
-  gulp.watch('app/scripts.babel/**/*.js', ['lint', 'babel']);
-  gulp.watch('bower.json', ['wiredep']);
-});
+  gulp.watch('app/scripts.babel/**/*.js', ['lint', 'babel'])
+  gulp.watch('bower.json', ['wiredep'])
+})
 
 gulp.task('size', () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}))
+})
 
 gulp.task('wiredep', () => {
   gulp.src('app/*.html')
     .pipe(wiredep({
       ignorePath: /^(\.\.\/)*\.\./
     }))
-    .pipe(gulp.dest('app'));
-});
+    .pipe(gulp.dest('app'))
+})
 
 gulp.task('package', function () {
-  var manifest = require('./dist/manifest.json');
+  var manifest = require('./dist/manifest.json')
   return gulp.src('dist/**')
       .pipe($.zip('HNView-' + manifest.version + '.zip'))
-      .pipe(gulp.dest('package'));
-});
+      .pipe(gulp.dest('package'))
+})
 
 gulp.task('build', (cb) => {
   runSequence(
     'lint', 'babel', 'chromeManifest',
     ['html', 'images', 'extras'],
-    'size', cb);
-});
+    'size', cb)
+})
 
 gulp.task('default', ['clean'], cb => {
-  runSequence('build', cb);
-});
+  runSequence('build', cb)
+})
